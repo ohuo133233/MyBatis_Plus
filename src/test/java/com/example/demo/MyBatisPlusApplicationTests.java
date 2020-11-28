bcabd132fb9a9fad67657236f3f56826e8e7bdb7
wangzhifengroot <1441289824@qq.com>
2020-11-29 02:01:38 +0800

    新增、增加、更新、删除方法

--- src/test/java/com/example/demo/MyBatisPlusApplicationTests.java
+++ src/test/java/com/example/demo/MyBatisPlusApplicationTests.java
@@ -12,16 +12,39 @@ import java.util.List;
 class MyBatisPlusApplicationTests {
     @Autowired
     private UserMapper userMapper;
+    // 查询
     @Test
-    void contextLoads() {
+    void selectList() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
+    // 插入
+    @Test
+    public void insert() {
         User user = new User();
         user.setName("1111");
         user.setEmail("1111");
         user.setAge(1);
         int insert = userMapper.insert(user);
-        System.out.println("insert: "+insert);
-        List<User> users = userMapper.selectList(null);
-        users.forEach(System.out::println);
+        System.out.println("insert: " + insert);
+        selectList();
     }
-
+    // 更新
+    @Test
+    public void update() {
+        User user = new User();
+        user.setName("222");
+        user.setEmail("222");
+        user.setAge(2);
+        user.setId(1L);
+        int updateById = userMapper.updateById(user);
+        System.out.println("updateById: " + updateById);
+        selectList();
+    }
+    // 删除
+    @Test
+    public void delete(){
+        userMapper.deleteById(3L);
+        selectList();
+    }
 }
