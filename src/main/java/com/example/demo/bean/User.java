4c449679c536907b53bcddbedba32da3f7479973
wangzhifengroot <1441289824@qq.com>
2020-11-29 22:50:41 +0800

    完善Demo

--- src/main/java/com/example/demo/bean/User.java
+++ src/main/java/com/example/demo/bean/User.java
@@ -2,19 +2,29 @@ package com.example.demo.bean;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.Version;
+
+import javax.xml.crypto.Data;
 
 public class User {
     @TableId(type = IdType.AUTO)
-    private Long  id;
+    private Long id;
     private String name;
     private Integer age;
     private String email;
+    private Data create_time;
+    private Data update_time;
+    @Version //乐观锁
+    private Integer version;
+    @TableLogic //逻辑删除
+    private Integer deleted;
 
-    public Long  getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long  id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -42,6 +52,39 @@ public class User {
         this.email = email;
     }
 
+
+    public void setCreate_time(Data create_time) {
+        this.create_time = create_time;
+    }
+
+    public Data getCreate_time() {
+        return create_time;
+    }
+
+    public void setUpdate_time(Data update_time) {
+        this.update_time = update_time;
+    }
+
+    public Data getUpdate_time() {
+        return update_time;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+    public Integer getDeleted() {
+        return deleted;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -49,6 +92,10 @@ public class User {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 ", email='" + email + '\'' +
+                ", create_time=" + create_time +
+                ", update_time=" + update_time +
+                ", version=" + version +
+                ", deleted=" + deleted +
                 '}';
     }
 }
--- src/test/java/com/example/demo/MyBatisPlusApplicationTests.java
+++ src/test/java/com/example/demo/MyBatisPlusApplicationTests.java
@@ -1,50 +1,116 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.bean.User;
 import com.example.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest
 class MyBatisPlusApplicationTests {
     @Autowired
     private UserMapper userMapper;
+
     // 查询
     @Test
     void selectList() {
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
     }
+
+
+    @Test
+    // 分页查询
+    public void testSelectPage() {
+        Page<User> userPage = new Page<>(2, 3);
+        Page<User> selectPage = userMapper.selectPage(userPage, null);
+        selectPage.getRecords().forEach(System.out::println);
+    }
+
+    @Test
+    public void selectById() {
+        User user = userMapper.selectById(6L);
+        System.out.println(user);
+    }
+
     // 插入
     @Test
     public void insert() {
         User user = new User();
-        user.setName("1111");
-        user.setEmail("1111");
-        user.setAge(1);
+        user.setName("666");
+        user.setEmail("666");
+        user.setAge(6);
         int insert = userMapper.insert(user);
         System.out.println("insert: " + insert);
         selectList();
     }
+
     // 更新
     @Test
     public void update() {
         User user = new User();
-        user.setName("222");
-        user.setEmail("222");
-        user.setAge(2);
+        user.setName("333");
+        user.setEmail("444");
+        user.setAge(4);
         user.setId(1L);
         int updateById = userMapper.updateById(user);
         System.out.println("updateById: " + updateById);
         selectList();
     }
+
+    // 乐观锁
+    @Test
+    public void testOptimisticLockerInnerInterceptor() {
+        User user = new User();
+        user.setName("333");
+        user.setEmail("444");
+        user.setAge(4);
+        user.setId(3L);
+
+        User user1 = new User();
+        user1.setName("555");
+        user1.setEmail("666");
+        user1.setAge(5);
+        user1.setId(3L);
+        int updateById1 = userMapper.updateById(user1);
+        int updateById = userMapper.updateById(user);
+        System.out.println("updateById1: " + updateById1);
+        System.out.println("updateById: " + updateById);
+        selectList();
+    }
+
+    @Test
+    // 多条件查询
+    public void testSelectMap() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "1111");
+        List<User> users = userMapper.selectByMap(map);
+        users.forEach(System.out::println);
+    }
+
+
     // 删除
     @Test
-    public void delete(){
-        userMapper.deleteById(3L);
-        selectList();
+    public void delete() {
+        int i = userMapper.deleteById(2L);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        userMapper.deleteBatchIds(list);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "333");
+        userMapper.deleteByMap(map);
+    }
+
+    // 逻辑删除
+    @Test
+    public void logicDelete() {
+        int i = userMapper.deleteById(6L);
+        System.out.println("logicDelete ID: " + i);
     }
 }
